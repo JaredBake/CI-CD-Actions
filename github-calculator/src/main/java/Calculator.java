817ac0d2d6f67c5d0b54508b96756128b0de67f6
Jared Bake <93339550+JaredBake@users.noreply.github.com>
2025-02-18 21:46:37 -0700

    updated binarynumber

--- github-calculator/src/main/java/Calculator.java
+++ github-calculator/src/main/java/Calculator.java
@@ -65,14 +65,16 @@ class Calculator {
             return "1010";
         }else if (number == 16){
             return "10000";
-        }else if (number == 20){
-            return "10100";
-        }else if (number == 999){
-            return "1111100111";
-        }else if (number == 1023){
-            return "1111111111";
+        }else {
+//        else if (number == 20){
+//            return "10100";
+//        }else if (number == 999){
+//            return "1111100111";
+//        }else if (number == 1023){
+//            return "1111111111";
+//        }
+            return Integer.toBinaryString(number);
         }
-        return null;
     }
 
     /*
