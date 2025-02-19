534f986dcf1bc418be50c35f7d6a1ca61d21005f
Jared Bake <93339550+JaredBake@users.noreply.github.com>
2025-02-18 23:03:11 -0700

    Update Main.java

--- github-calculator/src/main/java/Main.java
+++ github-calculator/src/main/java/Main.java
@@ -2,25 +2,25 @@ public class Main {
     public static void main(String[] args){
         System.out.println("entered main.java");
         Calculator test = new Calculator();
-        while (!args[0].equals("quit")) {
-            switch (args[0]) {
+        while (!args[1].equals("quit")) {
+            switch (args[1]) {
                 case "add":
-                    test.add(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+                    test.add(Integer.parseInt(args[2]), Integer.parseInt(args[3]));
                     break;
                 case "subtract":
-                    test.subtract(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+                    test.subtract(Integer.parseInt(args[2]), Integer.parseInt(args[3]));
                     break;
                 case "multiply":
-                    test.multiply(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+                    test.multiply(Integer.parseInt(args[2]), Integer.parseInt(args[3]));
                     break;
                 case "divide":
-                    test.divide(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+                    test.divide(Integer.parseInt(args[2]), Integer.parseInt(args[3]));
                     break;
                 case "binary":
-                    test.intToBinaryNumber(Integer.parseInt(args[1]));
+                    test.intToBinaryNumber(Integer.parseInt(args[2]));
                     break;
                 case "fibonacci":
-                    test.fibonacciNumberFinder(Integer.parseInt(args[1]));
+                    test.fibonacciNumberFinder(Integer.parseInt(args[2]));
                     break;
                 default:
                     System.out.println("Wrong command: Please type either 'add' 'subtract' 'multiply' 'divide' fibonacci' or 'binary'");
