8829c79d40d72f64081bc02e5619649326ebe9a1
Jared Bake <93339550+JaredBake@users.noreply.github.com>
2025-02-18 22:58:31 -0700

    Update Main.java

--- github-calculator/src/main/java/Main.java
+++ github-calculator/src/main/java/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Testing Dockerfile");
+        System.out.println("entered main.java");
+        Calculator test = new Calculator();
+        while (!args[0].equals("quit")) {
+            switch (args[0]) {
+                case "add":
+                    test.add(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+                    break;
+                case "subtract":
+                    test.subtract(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+                    break;
+                case "multiply":
+                    test.multiply(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+                    break;
+                case "divide":
+                    test.divide(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+                    break;
+                case "binary":
+                    test.intToBinaryNumber(Integer.parseInt(args[1]));
+                    break;
+                case "fibonacci":
+                    test.fibonacciNumberFinder(Integer.parseInt(args[1]));
+                    break;
+                default:
+                    System.out.println("Wrong command: Please type either 'add' 'subtract' 'multiply' 'divide' fibonacci' or 'binary'");
+            }
+        }
     }
 }
